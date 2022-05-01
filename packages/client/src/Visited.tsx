547254229431c3848107f637735ac72a531c733d
Magoo222 <magoo.1.9.2@gmail.com>
2022-05-01 18:20:27 +0100

    Visited and Wish List Pages working.  Needs styling.

--- packages/client/src/Visited.tsx
+++ packages/client/src/Visited.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import type { FC } from 'react'
-import { Container, Heading } from '@chakra-ui/react'
+import { Container, Heading, ListItem, UnorderedList } from '@chakra-ui/react'
 
-export const Visited: FC = () => (
-  <>
-    <Heading as="h1">Visited</Heading>
-    <Container centerContent maxW="container.md" flexDir="row"></Container>
-  </>
-)
+import { cityUrl } from './Home'
+
+export const Visited: FC = () => {
+  const [visited, setVisited] = useState()
+
+  useEffect(() => {
+    fetch(cityUrl, { method: 'GET' })
+      .then(response => response.json())
+      .then(data => setVisited(data))
+      .catch(error => console.log('Error getting data from API', error))
+  }, [])
+
+  const addListItems = () => {
+    const displayVisited = []
+
+    if (visited !== undefined) {
+      for (let i in visited.cities) {
+        if (visited.cities[i].visited === true) {
+          displayVisited.push(<ListItem>{visited.cities[i].name}</ListItem>)
+        }
+      }
+    }
+
+    return displayVisited
+  }
+
+  return (
+    <>
+      <Heading as="h1">Visited</Heading>
+      <Container centerContent maxW="container.md" flexDir="row"></Container>
+      <UnorderedList>{addListItems()}</UnorderedList>
+    </>
+  )
+}
--- packages/client/src/WishList.tsx
+++ packages/client/src/WishList.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import type { FC } from 'react'
-import { Container, Heading } from '@chakra-ui/react'
+import { Container, Heading, ListItem, UnorderedList } from '@chakra-ui/react'
 
-export const WishList: FC = () => (
-  <>
-    <Heading as="h1">Wish list</Heading>
-    <Container centerContent maxW="container.md" flexDir="row"></Container>
-  </>
-)
+import { cityUrl } from './Home'
+
+export const WishList: FC = () => {
+  const [wishlist, setWishlist] = useState()
+
+  useEffect(() => {
+    fetch(cityUrl, { method: 'GET' })
+      .then(response => response.json())
+      .then(data => setWishlist(data))
+      .catch(error => console.log('Error getting data from API', error))
+  }, [])
+
+  const addListItems = () => {
+    const displayWishlist = []
+
+    if (wishlist !== undefined) {
+      for (let i in wishlist.cities) {
+        if (wishlist.cities[i].wishlist === true) {
+          displayWishlist.push(<ListItem>{wishlist.cities[i].name}</ListItem>)
+        }
+      }
+    }
+
+    return displayWishlist
+  }
+
+  return (
+    <>
+      <Heading as="h1">Wish list</Heading>
+      <Container centerContent maxW="container.md" flexDir="row"></Container>
+      <UnorderedList>{addListItems()}</UnorderedList>
+    </>
+  )
+}
